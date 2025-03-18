f7c9ea781d7cadc31900be806d1aa62967a2646d
MoloKrakein <51546600+MoloKrakein@users.noreply.github.com>
2025-03-18 13:55:28 +0700

    Add CameraManager and PlayerManager scripts with initial setup; update Player prefab with new midPoint property

--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -27,11 +27,6 @@ public class Movement : MonoBehaviour
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; // Calculate the angle
         transform.rotation = Quaternion.Euler(0, 0, angle - 90); // Rotate the player
 
-    
-
-        
-
-
         
     }
 }
