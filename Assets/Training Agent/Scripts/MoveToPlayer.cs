97dd3a806cf4f5656ade33036be57f5f41d705c0
Alvin Tristandy <100253781+Alvintris@users.noreply.github.com>
2025-03-21 13:41:28 +0700

    Agent Update

--- Assets/Training Agent/Scripts/MoveToPlayer.cs
+++ Assets/Training Agent/Scripts/MoveToPlayer.cs
@@ -3,31 +3,24 @@ using System.Collections.Generic;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class MoveToPlayer : Agent
 {
     [Header("Agent Properties")]
-    [SerializeField] float moveSpeed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float moveSpeed;
 
     [Header("Player Reference")]
     [SerializeField] Transform playerPosition;
     [SerializeField] Transform[] spawnPoints;
 
-    [Header("Tilemap Color Properties")]
-    [SerializeField] Tilemap wall;
-    [SerializeField] Color whenSuccessColor;
-    [SerializeField] Color whenFailedColor;
-    [SerializeField] Color defaultColor;
-
     bool playerDetected;
     bool playerCaught;
     
     Vector3 startPosition;
-    Vector3 playerLastPosition;
-    float lastDistanceToPlayer;
+    Vector3 playerLastSeenPosition;
 
     RayPerceptionSensorComponent2D ray2d;
 
@@ -39,22 +32,18 @@ public class MoveToPlayer : Agent
 
     public override void OnEpisodeBegin()
     {
-        if (!playerCaught)
-        {
-            AddReward(-0.5f); // Penalti jika gagal menangkap pemain
+        if (!playerCaught){
+            AddReward(-1f);
         }
 
-        playerCaught = false;
         playerDetected = false;
-        wall.color = defaultColor;
+        playerCaught = false;
         transform.localPosition = startPosition;
 
         // Menentukan spawn pemain secara acak
         int randomIndex = Random.Range(0, spawnPoints.Length);
         Transform chosenSpawnPoint = spawnPoints[randomIndex];
         playerPosition.localPosition = chosenSpawnPoint.localPosition;
-
-        lastDistanceToPlayer = Vector3.Distance(transform.localPosition, playerPosition.localPosition);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -73,62 +62,54 @@ public class MoveToPlayer : Agent
             if (ray.HasHit && ray.HitGameObject.CompareTag("Player"))
             {
                 playerDetected = true;
-                playerLastPosition = ray.HitGameObject.transform.position;
-                AddReward(0.005f); // Reward lebih kecil agar agen tidak hanya melihat
+                playerLastSeenPosition = ray.HitGameObject.transform.localPosition;
                 break;
             }
         }
 
         sensor.AddObservation(playerDetected ? 1.0f : 0.0f);
-        sensor.AddObservation(playerDetected ? playerLastPosition.x : 0.0f);
-        sensor.AddObservation(playerDetected ? playerLastPosition.y : 0.0f);
+        sensor.AddObservation(playerDetected ? playerLastSeenPosition.x : 0.0f);
+        sensor.AddObservation(playerDetected ? playerLastSeenPosition.y : 0.0f);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        int walk = actions.DiscreteActions[0]; // 0 = diam, 1 = maju, 2 = mundur
-        int rotation = actions.DiscreteActions[1]; // 0 = tidak berputar, 1 = kiri, 2 = kanan
+        int rotation = actions.DiscreteActions[0]; // 0 = tidak berputar, 1 = kiri, 2 = kanan
+        int move = actions.DiscreteActions[1]; // 0 = diam, 1 = maju, 2 = mundur
 
         Vector3 moveDir = Vector3.zero;
         float rotationAmount = 0f;
 
-        if (walk == 1) moveDir = transform.up; // Maju
-        else if (walk == 2) moveDir = -transform.up; // Mundur (diberi penalti)
-
         if (rotation == 1) rotationAmount = rotationSpeed * Time.deltaTime;
         else if (rotation == 2) rotationAmount = -rotationSpeed * Time.deltaTime;
 
-        transform.localPosition += moveDir * moveSpeed * Time.deltaTime;
         transform.rotation = Quaternion.RotateTowards(transform.rotation,
             Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + rotationAmount),
             rotationSpeed * Time.deltaTime);
 
-        float currentDistance = Vector3.Distance(transform.localPosition, playerPosition.localPosition);
-        float distanceChange = lastDistanceToPlayer - currentDistance;
+        if (move == 1) moveDir = transform.up;
+        else if (move == 2) moveDir = -transform.up;
 
-        // **Reward jika agen mendekati pemain**
-        if (distanceChange > 0)
+        transform.localPosition += moveDir * moveSpeed * Time.deltaTime;
+
+        if (playerDetected)
         {
-            AddReward(distanceChange * 0.1f);
+            float distance = Vector3.Distance(transform.position, playerLastSeenPosition);
+            float reward = 0.002f * (1 / (distance + 1));
+            AddReward(reward);
         }
         else
         {
-            AddReward(-0.02f); // Penalti jika menjauh
+            AddReward(-0.01f);
         }
 
-        // **Penalti jika agen mundur saat melihat player**
-        if (playerDetected && walk == 2)
-        {
-            AddReward(-0.05f); // Penalti lebih besar jika mundur ke pemain
+        if(move == 1){
+            AddReward(0.003f);
         }
 
-        // **Penalti jika agen hanya berputar tanpa bergerak maju**
-        if (rotation != 0 && walk == 0)
-        {
-            AddReward(-0.02f);
+        if(move == 2){
+            AddReward(-0.01f);
         }
-
-        lastDistanceToPlayer = currentDistance;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -137,36 +118,32 @@ public class MoveToPlayer : Agent
         discreteActions[0] = 0;
         discreteActions[1] = 0;
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.A))
         {
             discreteActions[0] = 1;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.D))
         {
             discreteActions[0] = 2;
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
+        if(Input.GetKey(KeyCode.W)){
             discreteActions[1] = 1;
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
+        else if(Input.GetKey(KeyCode.S)){
             discreteActions[1] = 2;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            wall.color = whenSuccessColor;
+        if(collision.gameObject.CompareTag("Player")){
             playerCaught = true;
-            AddReward(2f);
+            AddReward(5f);
             EndEpisode();
         }
-        else if (collision.gameObject.CompareTag("Wall"))
-        {
+
+        if(collision.gameObject.CompareTag("Wall")){
             AddReward(-0.1f);
         }
     }
